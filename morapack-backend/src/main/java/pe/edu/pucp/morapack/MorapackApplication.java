8d6eaaea74e620b1bb2c7933174d34a22e3e42f1
BRYAN SMITH VALDIVIEZO JIMENEZ <bvaldiviezo@bancoripley.com.pe>
2025-09-22 19:58:53 -0500

    Primer push de backend

---
+++ morapack-backend/src/main/java/pe/edu/pucp/morapack/MorapackApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.morapack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorapackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorapackApplication.class, args);
+	}
+
+}
---
+++ morapack-backend/src/test/java/pe/edu/pucp/morapack/MorapackApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.morapack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorapackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
